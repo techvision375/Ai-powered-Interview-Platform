6022ac0b67e240f2bdf765190ca54615a2a9c476
tarun kumar <159427380+techvision375@users.noreply.github.com>
2025-08-28 22:58:07 +0530

    corret

--- Frontend/interview-prep-ai/src/App.jsx
+++ Frontend/interview-prep-ai/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Login from "./pages/Auth/Login.jsx";
-import Signup from "./pages/Auth/Signup.jsx";
+import Signup from "./pages/Auth/SignUp.jsx";
 import LandingPage from "./pages/LandingPage/LandingPage.jsx";
 import Dashboard from "./pages/home/Dashboard.jsx";
 import InterviewPrep from "./pages/interviewPrep/InterviewPrep.jsx";
